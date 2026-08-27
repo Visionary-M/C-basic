81ffb0a8a6029f0393c6f7807366a878af9a8d7b
Visionary-M <3022385319@qq.com>
2026-08-27 15:18:56 +0800

    8.27-first_commit

--- 9_for and if_2.c
+++ 9_for and if_2.c
@@ -1,6 +1,6 @@
 /*
     2026.8.26 14:05
-    目的：测试for语句使用
+    目的：测试for和if的嵌套使用
     功能：实现1-100中，奇数个数的输出
           实现1-100中，奇数和的输出
           实现1-100中，奇数平均值的输出
@@ -16,28 +16,31 @@ int main(void)
     int i;
     int sum_odd = 0;
     int sum_even = 0;
-    int count_odd = 0; 
+    int cnt_odd = 0; 
     float average = 0;
 
-    //奇数遍历求和
-    for (i=1; i<=100; i+=2)
+    //遍历分别求和
+    for (i=1; i<=100; i++)
     {
-        count_odd = count_odd + 1;
-        sum_odd = sum_odd + i;
+        if (i%2 == 1)
+        {
+        cnt_odd ++;
+        sum_odd += i;
+        }
+        else
+        {
+            sum_even += i;
+        }
+
     }
 
     //奇数平均值
-    average = (float)sum_odd / count_odd; 
-    //由于C中的除法特性，两个整型相除，只会的得到整数因此将sum_odd数值转化为浮点数
-
-    //偶数遍历求和
-    for (i=2; i<=100; i+=2)
-    {
-        sum_even = sum_even + i;
-    }
+    average = (float)sum_odd / cnt_odd; 
+    //由于C中的除法特性，两个整型相除，只会的得到整数,因此将sum_odd数值转化为浮点数
+    //简易写法：average = 1.0 * sum_odd / cnt_odd;
 
     //输出
-    printf("1-100之间的奇数个数是: %d\n", count_odd);
+    printf("1-100之间的奇数个数是: %d\n", cnt_odd);
     printf("1-100之间的奇数和是: %d\n", sum_odd);
     printf("1-100之间的奇数平均值是: %f\n", average);
     printf("1-100之间的偶数和是: %d\n", sum_even);
